bae085357014918f11b2628cd4eb7c29e5237dd3
Carlos Ramírez <cadavid4003@gmail.com>
2022-07-04 18:23:10 -0500

    prueba con google maps añadida

--- lib/screens/pantalla_mapa.dart
+++ lib/screens/pantalla_mapa.dart
@@ -3,25 +3,35 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 
-class PantallaMapa extends StatelessWidget {
-  PantallaMapa({Key? key}) : super(key: key);
+class PantallaMapa extends StatefulWidget {
+  const PantallaMapa({Key? key}) : super(key: key);
 
   @override
+  State<PantallaMapa> createState() => _PantallaMapaState();
+}
+
+class _PantallaMapaState extends State<PantallaMapa> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          obtenerPosicion();
-        },
-        child: Text('Get Location'),
-      ),
+    return FutureBuilder(
+      future: obtenerPosicionCamara(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Container(
+            child: GoogleMap(initialCameraPosition: snapshot.data),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
 
-obtenerPosicion() async {
-  LocationPermission permission = await Geolocator.requestPermission();
-  Position position = await Geolocator.getCurrentPosition(
+obtenerPosicionCamara() async {
+  await Geolocator.requestPermission();
+  Position posicion = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high);
-  print(position);
+  return CameraPosition(
+      target: LatLng(posicion.latitude, posicion.longitude), zoom: 16);
 }
